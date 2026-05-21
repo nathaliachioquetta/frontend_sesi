c47486dc884daf816af2370be8d55af263d7e2b2
nathaliachioquetta <nathalia.echioquetta@gmail.com>
2026-05-21 12:13:27 -0300

    continuação

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
-function executarSistema(){
-    // Dados de entrada
-    const nome = document.getElementById("inputNome") .value;
-    const idade = parseInt (document.getElementById("inputIdade").value);
-    const valor = parseFloat (document.getElementById("inputValor").value);
-    const cupom =document.getElementById("inputCupom").value =="true";
+function executarSistema() {
+    const nome = document.getElementById("inputNome").value;
+    const idade = parseInt(document.getElementById("inputIdade").value);
+    const valor = parseFloat(document.getElementById("inputValor").value);
+    const cupom = document.getElementById("inputCupom").value === "true";
 
-    // Dados de saída
+    //dados de saida
     const msg = document.getElementById("mensagem-autorizacao");
     const lista = document.getElementById("lista-estoque");
     const relatorio = document.getElementById("relatorio-final");
 
-    // Validação para campos vazios
-    if (!nome ||isNaN(idade)|| isNaN(valor)) {
-        alert("Por favor, preencha todos os campos");
-        return; 
+    // validação para campos vazios
+
+    if(!nome || isNaN(idade) || isNaN(valor)){
+        alert("O seu inergumeno, preenche os dados!");
+        return;
+    }
+    
+    // regra de negócio
+    if (idade >= 16){
+        msg.innerText = `Venda autorizada: ${nome}`;
+        msg.style.color = "#1aff00";
+
+        //desconto
+        let valorFinal = (valor > 500 || cupom) ? valor * 0.85 : valor;
+
+        //estoque
+        let estoque = ["placa de video", "processador", "memoria RAM"];
+        lista.innerHTML = ""; // limpa a lista anterior
+
+        estoque. forEach(Item => {
+            let li = document.createElement("li");
+        li.innerText = `Item) ${Item} reservado.`;
+        lista.appendChild(li);
+        });
+
+        //relatorio
+        relatorio.style.display = "block";
+        relatorio.innerHTML = `
+        <strong> RESUMO DO PEDIDO <\strong><br>
+        cliente: ${nome} <br>
+        Total Original: R$ ${valor.toFixed(2)} <br>
+        <strong> Total com Desconto: R$ ${valorFinal.toFixed(2)} <\strong>
+         `;
+
+    } else{
+        msg.innerText = "Venda bloqueada: Menor de 16 anos.";
+        msg.style.color = "#ff4444";
+        relatorio.style.display = "none";
+        lista.innerHTML = "";
     }
 
-    // Regra de negócio
-    if (idade >=16){
-        msg.innerText = `Venda autoriuzada: ${nome}`;
-        msg.style.color = "#00ff88"
+        
 
-        // Desconto
-        let valorFinal = (valor > 500 || cupom )? valor * 0.85 : valor;
-
-        // Estoque
-        let estoque = ["Placa de Vídeo", "Processador", "memória RAM"];
-        lista.innerHTML= ""; // Limpa a lista anterior
 
     }
-}
+ 
+
